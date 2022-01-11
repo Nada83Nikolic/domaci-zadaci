8c1c05d89ce61acd72b7c505c9d58e7e0ec2b8e7
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-11 17:40:50 +0100

    SmartAirConditioning

--- src/d10_01_2022/SmartAirConditioning.java
+++ src/d10_01_2022/SmartAirConditioning.java
@@ -1,5 +1,16 @@
 package d10_01_2022;
 
-public class SmartAirConditioning {
+public class SmartAirConditioning 
+{
+String naziv;
+int temp;
+String mod;
 
+
+public void stampaj()
+{
+	System.out.println(this.naziv+" klima ");
+	System.out.println(this.mod+ "do ");
+	System.out.println(this.temp+ " stepeni.");
+}
 }
