c47d671a316f630038d56ddfd2cdfef88814a2c2
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-15 15:49:20 +0100

    init fb

---
+++ src/d_13_01_22/FacebookPost.java
@@ -0,0 +1,5 @@
+package d_13_01_22;
+
+public class FacebookPost {
+
+}
---
+++ src/d_13_01_22/Zadatak_1_fbPost.java
@@ -0,0 +1,10 @@
+package d_13_01_22;
+
+public class Zadatak_1_fbPost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
