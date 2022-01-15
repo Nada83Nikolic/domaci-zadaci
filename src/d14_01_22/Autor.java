3cf08c8a3024406e0a496e175dd759695ac78deb
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-15 21:16:30 +0100

    Autor

--- src/d14_01_22/Autor.java
+++ src/d14_01_22/Autor.java
@@ -1,5 +1,26 @@
 package d14_01_22;
 
 public class Autor {
-
+	private String ime;
+	private String prezime;
+	
+	public Autor(String ime,String prezime)
+	{
+		this.ime=ime;
+		this.prezime=prezime;
+	}
+	
+	public String getIme()
+	{
+		return this.ime;
+	}
+	public String getPrezime()
+	{
+		return this.prezime;
+	}
+	
+	public void print()
+	{
+		System.out.println(this.ime+ " "+this.prezime);
+	}
 }
