abe54e16e06375cbe16c3b76c610334f684cbfa8
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-15 21:17:13 +0100

    Knjiga

--- src/d14_01_22/Knjiga.java
+++ src/d14_01_22/Knjiga.java
@@ -1,5 +1,46 @@
 package d14_01_22;
 
 public class Knjiga {
-
+	private String isbn;
+	private String naziv;
+	private int godinaIzdanja;
+	private Autor autor;
+	
+	public Knjiga(String isbn,String naziv,int godinaIzdanja) {
+		
+		this.isbn=isbn;
+		this.naziv=naziv;
+		this.godinaIzdanja=godinaIzdanja;
+		
+	}
+	 
+	public String getIsbn()
+	{
+		return this.isbn;
+	}
+	public String getNaziv()
+	{
+		return this.naziv;
+	}
+	public int getGodinaIzdanja()
+	{
+		return this.godinaIzdanja;
+	}
+	
+	public Autor getAutor()
+	{
+		return this.getAutor();
+	}
+	public void setAutor(Autor autor)
+	{
+		this.autor=autor;
+	}
+	public void stampaj()
+	{
+		System.out.println(this.isbn);
+		System.out.println(this.naziv+" - "+ this.godinaIzdanja);
+		System.out.print("Autor: ");
+		this.autor.print();
+		System.out.println();
+	}
 }
