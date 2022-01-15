11a4cfc7a4ecec0dccb10dbda736e5d66fec97c6
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-15 21:17:47 +0100

    AutorKnjige

--- src/d14_01_22/AutorKnjige.java
+++ src/d14_01_22/AutorKnjige.java
@@ -3,8 +3,19 @@ package d14_01_22;
 public class AutorKnjige {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Knjiga k1 = new Knjiga ("ISBN: 86-7436-045-9","Kamionci",2002);
+		Autor autor1 = new Autor("Teri","Pracet");
+		
+		k1.setAutor(autor1);	
+		k1.stampaj();
 
+		
+		Knjiga k2 = new Knjiga("ISBN: 978-86-521-3107-5","Severna svetlost",2018);
+		Autor autor2=new Autor("Filip","Pulman");
+		
+		k2.setAutor(autor2);
+		k2.stampaj();
 	}
 
 }
