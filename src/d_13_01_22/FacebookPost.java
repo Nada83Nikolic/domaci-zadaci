2b29d2b4960b9188146e46ac66688213f0da325c
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-15 20:05:24 +0100

    FB post

--- src/d_13_01_22/FacebookPost.java
+++ src/d_13_01_22/FacebookPost.java
@@ -1,5 +1,58 @@
 package d_13_01_22;
 
 public class FacebookPost {
+	private String imeKo;
+	private String imeKome;
+	private String text;
+	private int brojLajkova;
+	private int brojDeljenja;
 
+
+	public FacebookPost(String imeKo, String imeKome, String text) {
+		this.imeKo = imeKo;
+		this.imeKome = imeKome;
+		this.text = text;
+	}
+
+	public String getImeKo() {
+		return this.imeKo;
+	}
+
+	public String getImeKome() {
+		return this.imeKome;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	public void setBrojLajkova(int brojLajkova) {
+		this.brojLajkova = brojLajkova;
+	}
+
+	public void like() {
+		this.brojLajkova = this.brojLajkova +1;
+	}
+	public void dislike() {
+	
+		this.brojLajkova=this.brojLajkova-1;
+		
+		if(this.brojLajkova<0)
+		{
+			this.brojLajkova=0;}
+	}
+	public void share() {
+		this.brojDeljenja=this.brojDeljenja+1;
+	}
+	public void print()
+	{
+		System.out.println(this.imeKo+ " >>> "+ this.imeKome);
+		System.out.println(this.text);
+		System.out.println("Likes "+this.brojLajkova+ "  |  Shares "+this.brojDeljenja);
+	}
 }
--- src/d_13_01_22/Zadatak_1_fbPost.java
+++ src/d_13_01_22/Zadatak_1_fbPost.java
@@ -1,10 +1,39 @@
 package d_13_01_22;
 
+
 public class Zadatak_1_fbPost {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		FacebookPost k1= new FacebookPost("Nada Nikolic","Drustvo sa kursa","Najjaci smo :D");
+		k1.like();
+		k1.like();
+		k1.like();
+		k1.like();
+		k1.like();
+		k1.like();
+		k1.dislike();
+		k1.dislike();
+		k1.share();
+		k1.dislike();
+		k1.print();
+	
+	System.out.println();
+		FacebookPost k2= new FacebookPost("Nada Nikolic","Neko tamo", "Keep smiling :D");
+	
+		k2.like();
+		k2.like();
+		k2.like();
+		k2.like();
+		k2.like();
+		k2.like();
+		k2.share();
+		k2.share();
+		k2.dislike();
+		k2.print();
+	
+		
 	}
 
 }
