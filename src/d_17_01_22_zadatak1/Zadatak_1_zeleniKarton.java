23bac37bf535859167a9104b53a880b924d9838a
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-17 23:26:00 +0100

    init Zeleni Karton

---
+++ src/d_17_01_22_zadatak1/Zadatak_1_zeleniKarton.java
@@ -0,0 +1,10 @@
+package d_17_01_22_zadatak1;
+
+public class Zadatak_1_zeleniKarton {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/d_17_01_22_zadatak1/ZeleniKarton.java
@@ -0,0 +1,5 @@
+package d_17_01_22_zadatak1;
+
+public class ZeleniKarton {
+
+}
