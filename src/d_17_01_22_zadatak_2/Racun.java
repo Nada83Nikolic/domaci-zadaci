b9edb8ddb9f2d5b599b7b1944c4de772f71c91ef
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-18 22:47:28 +0100

    TransakcijaRacun

---
+++ src/d_17_01_22_zadatak_2/Racun.java
@@ -0,0 +1,5 @@
+package d_17_01_22_zadatak_2;
+
+public class Racun {
+
+}
---
+++ src/d_17_01_22_zadatak_2/Transakcija_Racun.java
@@ -0,0 +1,10 @@
+package d_17_01_22_zadatak_2;
+
+public class Transakcija_Racun {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
