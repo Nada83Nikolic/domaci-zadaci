d0919fa2930f4a5aeadaa5314d99ed72c9f1c342
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-18 17:08:00 +0100

    ZeleniKarton

--- src/d_17_01_22_zadatak1/Zadatak_1_zeleniKarton.java
+++ src/d_17_01_22_zadatak1/Zadatak_1_zeleniKarton.java
@@ -1,10 +1,36 @@
 package d_17_01_22_zadatak1;
 
+import java.util.ArrayList;
+
 public class Zadatak_1_zeleniKarton {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ArrayList<ZeleniKarton> zk = new ArrayList<ZeleniKarton>();
+
+		ZeleniKarton z1 = new ZeleniKarton("Nada", "Nikolic", "Petar", "Petrovic", "1078/2015", "matematika", 6);
+		ZeleniKarton z2 = new ZeleniKarton("Nikola", "Jovic", "Petar", "Petrovic", "1075/2015", "matematika", 5);
+		ZeleniKarton z3 = new ZeleniKarton("Marina", "Joksimovic", "Jovan", "Savic", "1034/2001", "Srpski", 9);
+		ZeleniKarton z4 = new ZeleniKarton("Marko", "Jokic", "Stevan", "Popovic", "1084/1995", "Srpski", 8);
+		ZeleniKarton z5 = new ZeleniKarton("Sanja", "Vasic", "Ilija", "Jovanovic", "1257/1999", "Engleski", 5);
+		
+		
+		zk.add(z1);
+		zk.add(z2);
+		zk.add(z3);
+		zk.add(z4);
+		zk.add(z5);
+		double prosOcena = 0;
+		int brojac = 0;
+		for (int i = 0; i < zk.size(); i++) {
+			zk.get(i).print();
+
+			if (zk.get(i).daLiJePolozio()) {
+				brojac++;
+				prosOcena = (prosOcena + zk.get(i).getOcena());
+			}
+		}
+		System.out.println("Prosecna ocena polozenih ispita je: " + prosOcena / brojac);
 	}
 
 }
--- src/d_17_01_22_zadatak1/ZeleniKarton.java
+++ src/d_17_01_22_zadatak1/ZeleniKarton.java
@@ -1,5 +1,92 @@
 package d_17_01_22_zadatak1;
 
 public class ZeleniKarton {
+	private String imeStudenta;
+	private String prezimeStudenta;
+	private String imeProfesora;
+	private String prezimeProfesora;
+	private String brojIndexa;
+	private String nazivPredmeta;
+	private int ocena;
+	double prosecnaOcena;
+	public ZeleniKarton() {
+		
+	}
+
+	public ZeleniKarton(String imeStudenta, String prezimeStudenta,String imeProfesora,String prezimeProfesora, String brojIndexa, String nazivPredmeta,
+			int ocena) {
+		
+		this.imeStudenta = imeStudenta;
+		this.prezimeStudenta = prezimeStudenta;
+		this.imeProfesora=imeProfesora;
+		this.prezimeProfesora=prezimeProfesora;
+		this.brojIndexa = brojIndexa;
+		this.nazivPredmeta = nazivPredmeta;
+		this.ocena = ocena;
+	}
+
+	public String getNazivPredmeta() {
+		return nazivPredmeta;
+	}
+
+	public void setNazivPredmeta(String nazivPredmeta) {
+		this.nazivPredmeta = nazivPredmeta;
+	}
+
+	public int getOcena() {
+		return ocena;
+	}
+
+	public void setOcena(int ocena) {
+		this.ocena = ocena;
+	}
+
+	public String getImeStudenta() {
+		return imeStudenta;
+	}
+
+	public String getPrezimeStudenta() {
+		return prezimeStudenta;
+	}
+
+	public String getImeProfesora() {
+		return imeProfesora;
+	}
+
+	public void setImeProfesora(String imeProfesora) {
+		this.imeProfesora = imeProfesora;
+	}
+
+	public String getPrezimeProfesora() {
+		return prezimeProfesora;
+	}
+
+	public void setPrezimeProfesora(String prezimeProfesora) {
+		this.prezimeProfesora = prezimeProfesora;
+	}
+
+	public String getBrojIndexa() {
+		return brojIndexa;
+	}
+	public boolean daLiJePolozio()
+	{
+		return this.ocena>5;
+	}
+	
+	public void print()
+	{
+		System.out.println(this.nazivPredmeta+" - "+this.ocena+"\nStudent: "+this.imeStudenta+" "+this.prezimeStudenta);
+		System.out.println("Profesor: "+this.imeProfesora+" "+this.prezimeProfesora);
+		if(this.daLiJePolozio()==true)
+		{
+			System.out.println("Ispit je polozen.");
+		}
+		else
+		{
+			System.out.println("Ispit nije polozen.");
+		}
+		System.out.println();
+	}
+	
 
 }
