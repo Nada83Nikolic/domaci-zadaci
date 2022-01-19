468b39fffb17a0dda15de6e428f4ccac329b504b
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-19 17:11:02 +0100

    init StudentIspit

---
+++ src/d_18_01_22_zadatak_1/Ispit.java
@@ -0,0 +1,5 @@
+package d_18_01_22_zadatak_1;
+
+public class Ispit {
+
+}
---
+++ src/d_18_01_22_zadatak_1/Zadatak_1.java
@@ -0,0 +1,9 @@
+package d_18_01_22_zadatak_1;
+
+public class Zadatak_1 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
