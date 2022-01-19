eac0e15b7a08af09e8c6771ac4b0a085ffff6256
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-19 17:11:45 +0100

    Transakcija..

--- src/d_17_01_22_zadatak_2/Transakcija.java
+++ src/d_17_01_22_zadatak_2/Transakcija.java
@@ -5,64 +5,66 @@ public class Transakcija {
 	private Racun racunSaKog;
 	private Racun racunNaKoji;
 
-	
 	public Transakcija() {
-	
+
 	}
+
 	public Transakcija(String idTransakcije, Racun racunSaKog, Racun racunNaKoji) {
-		
+
 		this.idTransakcije = idTransakcije;
 		this.racunSaKog = racunSaKog;
 		this.racunNaKoji = racunNaKoji;
 	}
+
 	public String getIdTransakcije() {
 		return idTransakcije;
 	}
+
 	public void setIdTransakcije(String idTransakcije) {
 		this.idTransakcije = idTransakcije;
 	}
+
 	public Racun getRacunSaKog() {
 		return racunSaKog;
 	}
+
 	public void setRacunSaKog(Racun racunSaKog) {
 		this.racunSaKog = racunSaKog;
 	}
+
 	public Racun getRacunNaKoji() {
 		return racunNaKoji;
 	}
+
 	public void setRacunNaKoji(Racun racunNaKoji) {
 		this.racunNaKoji = racunNaKoji;
 	}
-	private int vratiProviziju(int iznos)
-	{
-		int provizija=0;
-		if(iznos<4500)
-		{
-			 provizija=45;
+
+	private int vratiProviziju(int iznos) {
+		int provizija = 0;
+		if (iznos < 4500) {
+			provizija = 45;
 		}
-		if(iznos>4500)
-		{
-			provizija=iznos/100;
+		if (iznos > 4500) {
+			provizija = iznos / 100;
 		}
 		return provizija;
 	}
-	public void izvrsiTransakciju(int vrednost)
-	{
-		if (this.racunSaKog.getTrenutnoStanje()-vrednost-this.vratiProviziju(vrednost)>0)
-		{
-			
-			System.out.println("Racuna sa: "+this.racunSaKog.getImeIPrezime()+" " +this.racunSaKog.getBrojRacuna());
-			System.out.println(this.racunSaKog.promeniStanje(vrednost)-this.vratiProviziju(vrednost));
-	
-			System.out.println("Racun na: "+this.racunNaKoji.getImeIPrezime()+" " +this.racunNaKoji.getBrojRacuna());
-			System.out.println(this.racunNaKoji.promeniStanje(-vrednost));
-		}
-		else
-		{
+
+	public void izvrsiTransakciju(int vrednost) {
+		if (this.racunSaKog.getTrenutnoStanje() - vrednost - this.vratiProviziju(vrednost) > 0) {
+
+			System.out
+					.println("Racuna sa: " + this.racunSaKog.getImeIPrezime() + " " + this.racunSaKog.getBrojRacuna());
+			System.out.println(this.racunSaKog.promeniStanje(vrednost) - this.vratiProviziju(vrednost));
+
+			System.out
+					.println("Racun na: " + this.racunNaKoji.getImeIPrezime() + " " + this.racunNaKoji.getBrojRacuna());
+			System.out.println(this.racunNaKoji.promeniStanje(-vrednost)); // dodajem mu na ovaj nacin
+		} else {
 			System.out.println("Nemate dovoljno sredstava da izvrsite zeljenu transakciju.");
 		}
-		
+
 	}
-	
 
 }
--- src/d_17_01_22_zadatak_2/Transakcija_Racun.java
+++ src/d_17_01_22_zadatak_2/Transakcija_Racun.java
@@ -3,21 +3,17 @@ package d_17_01_22_zadatak_2;
 public class Transakcija_Racun {
 
 	public static void main(String[] args) {
-	Racun r1=new Racun("51544-5154","Nada Nikolic",150000);
-	Racun r2=new Racun("152-246-326","Pera Peric",15000);
-	Transakcija t1=new Transakcija();
-	t1.setRacunSaKog(r1);
-	t1.setRacunNaKoji(r2);
-	
-//	r1.promeniStanje(150);
-	
-	r1.print();
-	r2.print();
-	
-	
-	t1.izvrsiTransakciju(149000);
-	
-	
+		Racun r1 = new Racun("51544-5154", "Nada Nikolic", 150000);
+		Racun r2 = new Racun("152-246-326", "Pera Peric", 15000);
+		Transakcija t1 = new Transakcija();
+		t1.setRacunSaKog(r1);
+		t1.setRacunNaKoji(r2);
+
+		r1.print();
+		r2.print();
+
+		t1.izvrsiTransakciju(149000);
+
 	}
 
 }
