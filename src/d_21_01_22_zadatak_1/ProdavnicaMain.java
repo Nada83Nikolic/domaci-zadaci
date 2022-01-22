b49c15a43322f532ed5fb3e6abd7f3c14643a403
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-22 20:12:52 +0100

    init 21_01_22_prodavnica

---
+++ src/d_21_01_22_zadatak_1/ProdavnicaMain.java
@@ -0,0 +1,10 @@
+package d_21_01_22_zadatak_1;
+
+public class ProdavnicaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/d_21_01_22_zadatak_1/Radnik.java
@@ -0,0 +1,5 @@
+package d_21_01_22_zadatak_1;
+
+public class Radnik {
+
+}
