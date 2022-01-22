97f9d1c2cb4a95b0d7882a25277a0c9c400c66ad
Nada83Nikolic <nada83nikolic@gmail.com>
2022-01-22 18:01:49 +0100

    init Sport

---
+++ src/d_20_01_22_zadatak_1/Osoba.java
@@ -0,0 +1,5 @@
+package d_20_01_22_zadatak_1;
+
+public class Osoba {
+
+}
---
+++ src/d_20_01_22_zadatak_1/SportMain.java
@@ -0,0 +1,10 @@
+package d_20_01_22_zadatak_1;
+
+public class SportMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
